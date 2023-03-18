6abe23f9e60b84b5987b0aaeb9383517f8133970
mohammediris <mohammedirisa@gmail.com>
2023-03-18 23:13:54 +0400

    my initial files

---
+++ pythonCourse.py
@@ -0,0 +1,11 @@
+# User input
+# user = input("Please enter the string you want to be printed out: ")
+
+# Saving the input to a variable
+# user_says = input("Please enter the string you want to be printed out: ")
+# print(user_says)
+
+# variable assignment. multiple variables can be assigned in single line
+a, b, c = 1, 2, 3
+print(a,b,c)
+print(id(a)) # id function will show the location of the assigned value
